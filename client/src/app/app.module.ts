c608850b9cb0cb810bbd26045c5e31451cdf0d00
Salvadorvga2004 <116208397+Salvadorvga2004@users.noreply.github.com>
2024-03-16 01:42:34 -0600

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AutosComponent } from './componentes/autos/autos.component';
+import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AutosComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
