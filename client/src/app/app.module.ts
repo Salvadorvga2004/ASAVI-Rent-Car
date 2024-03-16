6dfb4e24eba902782186e7cbf199bfc6438ccc3c
Salvadorvga2004 <116208397+Salvadorvga2004@users.noreply.github.com>
2024-03-16 01:17:42 -0600

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
-import { AppRouting } from './app.routes';
 
 import { AppComponent } from './app.component';
-import { AutosComponent } from './componentes/autos/autos.component';
-import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AutosComponent,
-    UsuariosComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
-    AppRouting
+    RouterModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
