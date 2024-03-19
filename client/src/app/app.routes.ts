1c870d3812307831702d8ec9b5175d98b1f0149c
Salvadorvga2004 <116208397+Salvadorvga2004@users.noreply.github.com>
2024-03-19 11:23:53 -0600

    Revert "Revert "Ivana - componentes""
    This reverts commit 0f660dfc65db4efa8de5a8616a616684b8fcd28e.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { PantalladosComponent } from './pantallados/pantallados.component';
+import { PromocionesComponent } from './promociones/promociones.component';
+import { DestinoComponent } from './destino/destino.component';
+
 
 export const routes: Routes = [
     {
@@ -17,4 +21,18 @@ export const routes: Routes = [
         path:'inicio',
         component: InicioComponent
     },
+    {
+        path:'Pantalla-dos',
+        component: PantalladosComponent
+    },
+    {
+        path:'promociones',
+        component:PromocionesComponent
+    },
+    {
+        path: 'destino',
+        component:DestinoComponent
+    }
+
+
 ];
---
+++ client/src/app/pantallados/pantallados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantallados',
+  standalone: true,
+  imports: [],
+  templateUrl: './pantallados.component.html',
+  styleUrl: './pantallados.component.css'
+})
+export class PantalladosComponent {
+
+}
