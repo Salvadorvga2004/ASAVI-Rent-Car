3b9545141863b597afa89fc8560b9f5373bdae73
Salvadorvga2004 <116208397+Salvadorvga2004@users.noreply.github.com>
2024-03-19 20:55:46 -0600

    Acomodo

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
+
+
 import { AppComponent } from './app.component';
+import { AutosComponent } from './componentes/autos/autos.component';
+import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    AutosComponent,
+    UsuariosComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/componentes/usuarios/usuarios.component.ts
+++ client/src/app/componentes/usuarios/usuarios.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-usuarios',
-  standalone: true,
   imports: [],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
